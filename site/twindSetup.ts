b552c6619d36e97a11ddf2d8d1ca58a18acf65c8
Juho Vepsäläinen <bebraw@gmail.com>
2022-11-17 20:32:35 +0200

    chore: Set up font + initial header

--- site/twindSetup.ts
+++ site/twindSetup.ts
@@ -1,8 +1,16 @@
 import * as twindColors from "https://cdn.skypack.dev/twind@0.16.16/colors?min";
 import twindTypography from "https://cdn.skypack.dev/@twind/typography@0.0.2?min";
 
+// https://twind.dev/handbook/configuration.html#custom-fonts-and-imports
 export default {
-  theme: { extend: { colors: twindColors } },
+  theme: {
+    extend: {
+      colors: twindColors,
+      fontFamily: {
+        "avenir-next": '"Avenir Next"',
+      },
+    },
+  },
   plugins: {
     ...twindTypography(),
   },
