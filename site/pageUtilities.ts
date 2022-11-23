163fd1c794dd82cca1d7e9f03e07c4da7e9d0621
Juho Vepsäläinen <bebraw@gmail.com>
2022-11-23 11:51:03 +0200

    feat: Add internal url validation

--- site/pageUtilities.ts
+++ site/pageUtilities.ts
@@ -1,4 +1,5 @@
 import md from "./transforms/markdown.ts";
+import routes from "./routes.json" assert { type: "json" };
 import type { Context } from "https://deno.land/x/gustwind@v0.30.1/breezewind/types.ts ";
 
 function getDate(_: Context, d: string) {
@@ -15,4 +16,21 @@ function markdown(_: Context, input: string) {
   return md(input).content;
 }
 
-export { getDate, getYear, markdown };
+function validateUrl(_: Context, url: string) {
+  // Note that this doesn't support nested routes. Probably Gustwind
+  // core should expose a helper for this type of check.
+  if (Object.keys(routes).includes(url)) {
+    return url === "/" ? "/" : `/${url}/`;
+  }
+
+  // TODO: This would be a good spot to check the url doesn't 404
+  // To keep this fast, some kind of local, time-based cache would
+  // be good to have to avoid hitting the urls all the time.
+  if (url.startsWith("http")) {
+    return url;
+  }
+
+  throw new Error(`Failed to find matching url for "${url}"`);
+}
+
+export { getDate, getYear, markdown, validateUrl };
