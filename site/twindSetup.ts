ae7694f5aa41d6ca9de9a0d56c2680c430ece2ae
Juho Vepsäläinen <bebraw@gmail.com>
2022-12-23 11:43:38 +0200

    feat: Clean up and add robots.txt

--- site/twindSetup.ts
+++ site/twindSetup.ts
@@ -31,44 +31,8 @@ export default {
       {
         fontFamily: "Eau",
         fontWeight: "400",
-        src: 'url(/assets/fonts/eau_sans_book.woff2) format("woff2")',
+        src: 'url(/fonts/eau_sans_book.woff2) format("woff2")',
       },
     ],
   },
 };
-
-/*
-
-import * as twindColors from "https://cdn.skypack.dev/twind@0.16.16/colors?min";
-import twindTypography from "https://cdn.skypack.dev/@twind/typography@0.0.2?min";
-
-// https://twind.dev/handbook/configuration.html#custom-fonts-and-imports
-export default {
-  theme: {
-    extend: {
-      colors: {
-        ...twindColors,
-        primary: "#0f0349", // "#4a25f5",
-        secondary: "#84ebec",
-        tertiary: "#d239f6",
-      },
-      fontFamily: {
-        "primary": '"Eau"',
-        "monospace": "monospace",
-      },
-    },
-  },
-  preflight: {
-    "@font-face": [
-      {
-        fontFamily: "Eau",
-        fontWeight: "400",
-        src: 'url(/assets/fonts/eau_sans_book.woff2) format("woff2")',
-      },
-    ],
-  },
-  plugins: {
-    ...twindTypography(),
-  },
-};
-*/
