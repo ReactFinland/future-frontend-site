5c31b65a48a7755c1ac833c43476aea442a3351b
Juho Vepsäläinen <bebraw@gmail.com>
2022-12-27 16:33:05 +0200

    feat: Defer map loading

---
+++ site/scripts/venue.ts
@@ -0,0 +1,26 @@
+import "https://cdn.jsdelivr.net/npm/leaflet@1.9.3/dist/leaflet-src.min.js";
+
+function renderMap(id: string) {
+  // @ts-expect-error How to type Leaflet?
+  const L = window.L;
+  const venueLatLon = [60.168413, 24.949216];
+  const map = L.map(id).setView(venueLatLon, 14);
+
+  // Different options at https://leaflet-extras.github.io/leaflet-providers/preview/
+  L.tileLayer(
+    "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+    {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: "abcd",
+      maxZoom: 20,
+    },
+  ).addTo(map);
+
+  L.marker(venueLatLon).addTo(map)
+    // .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+    .bindPopup("Pörssitalo, Helsinki Stock Exchange Building")
+    .openPopup();
+}
+
+renderMap("venue-map");
