47ef2e936cc41dd40295403a7ffc83b209d70abf
Juho Vepsäläinen <bebraw@gmail.com>
2023-01-03 16:23:06 +0200

    chore: Add a todo

--- generate-og.js
+++ generate-og.js
@@ -1,11 +1,8 @@
 const sharp = require("sharp")
 
+// TODO: Use breezewind template to generate og.png for each route within the
+// build directory. Maybe a good option would be to ask all routes from
+// gustwind first.
 sharp("./complex.svg")
   .png()
   .toFile("./build/og.png")
-  .then(function(info) {
-    console.log(info)
-  })
-  .catch(function(err) {
-    console.log(err)
-  })
