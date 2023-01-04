5d42fcb5a557ae9d3c48b0f9101694b64f219ede
Juho Vepsäläinen <bebraw@gmail.com>
2023-01-04 11:32:38 +0200

    feat: Add og generation pass

--- generate-og.js
+++ generate-og.js
@@ -1,8 +1,32 @@
+const path = require('path')
+const { default: breeze } = require('breezewind')
 const sharp = require("sharp")
+const routes = require('./build/routes.json')
+const ogTemplate = require('./site/layouts/og.json')
+const meta = require('./site/meta.json')
 
-// TODO: Use breezewind template to generate og.png for each route within the
-// build directory. Maybe a good option would be to ask all routes from
-// gustwind first.
-sharp("./complex.svg")
-  .png()
-  .toFile("./build/og.png")
+async function generateOpengraphImages() {
+  for await (const [name, route] of Object.entries(routes)) {
+    if (!name.endsWith('.html') && !name.endsWith('.xml')) {
+      const svg = await breeze({
+        component: ogTemplate,
+        components: {},
+        context: { ...meta, meta: { title: 'Future Frontend', subtitle: route.meta.title }}
+      })
+      const outputPath = path.join(process.cwd(), 'build', name, 'og.png')
+
+      await sharp(Buffer.from(svg)).png().toFile(outputPath)
+    }
+  }
+
+  // TODO: Use breezewind template to generate og.png for each route within the
+  // build directory. Maybe a good option would be to ask all routes from
+  // gustwind first.
+  /*
+  sharp("./complex.svg")
+    .png()
+    .toFile("./build/og.png")
+  */
+}
+
+generateOpengraphImages();
