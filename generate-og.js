a65a8bc3b43c090ee21f2202652f9a501abcf77d
Juho Vepsäläinen <bebraw@gmail.com>
2023-01-04 11:37:51 +0200

    chore: Drop dead code

--- generate-og.js
+++ generate-og.js
@@ -18,15 +18,6 @@ async function generateOpengraphImages() {
       await sharp(Buffer.from(svg)).png().toFile(outputPath)
     }
   }
-
-  // TODO: Use breezewind template to generate og.png for each route within the
-  // build directory. Maybe a good option would be to ask all routes from
-  // gustwind first.
-  /*
-  sharp("./complex.svg")
-    .png()
-    .toFile("./build/og.png")
-  */
 }
 
 generateOpengraphImages();
