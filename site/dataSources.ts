f9aa5a59678ea83caf3efe04981447bbc69df535
Juho Vepsäläinen <bebraw@gmail.com>
2023-01-23 11:11:46 +0200

    chore: Add a launch blog post

--- site/dataSources.ts
+++ site/dataSources.ts
@@ -1,13 +1,24 @@
 import { GraphQLRequest } from "https://deno.land/x/gql_request@1.0.0-beta.2/mod.ts";
 import { get } from "https://deno.land/x/gustwind@v0.32.0/utilities/functional.ts";
+import { parse } from "https://deno.land/x/frontmatter@v0.1.4/mod.ts";
+import { dir } from "https://deno.land/x/gustwind@v0.39.1/utilities/fs.ts";
 import indexQuery from "./queries/indexQuery.ts";
 import organizersQuery from "./queries/organizersQuery.ts";
 import scheduleQuery from "./queries/scheduleQuery.ts";
 import speakersQuery from "./queries/speakersQuery.ts";
 import workshopsQuery from "./queries/workshopsQuery.ts";
 
-const CONFERENCE = "future-frontend-2023";
+type MarkdownWithFrontmatter = {
+  data: {
+    slug: string;
+    title: string;
+    date: Date;
+    keywords: string[];
+  };
+  content: string;
+};
 
+const CONFERENCE = "future-frontend-2023";
 const fetchData = createDataFetcher("https://api.react-finland.fi/graphql");
 
 function createDataFetcher(apiUrl: string) {
@@ -47,4 +58,14 @@ function readFile(filename: string) {
   return Deno.readTextFile(filename);
 }
 
-export { queryData, readFile };
+async function indexMarkdown(directory: string) {
+  const files = await dir({ path: directory, extension: ".md" });
+
+  return Promise.all(
+    files.map(({ path }) =>
+      Deno.readTextFile(path).then((d) => parse(d) as MarkdownWithFrontmatter)
+    ),
+  );
+}
+
+export { indexMarkdown, queryData, readFile };
