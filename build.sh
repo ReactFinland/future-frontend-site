d6b4d55137f0d12bb55cb32bcb4ff375fefd31b4
Juho Vepsäläinen <bebraw@gmail.com>
2023-01-30 20:31:23 +0200

    chore: Bump Deno version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export FONTCONFIG_PATH=${PWD}/assets/fonts
-curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.28.1
+curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.30.0
 /opt/buildhome/.deno/bin/deno task build
 npm run generate:og
