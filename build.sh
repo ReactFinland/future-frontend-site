96db29fedb151f7f8cc0c7677a8c8b58252470c8
Juho Vepsäläinen <bebraw@gmail.com>
2023-01-31 09:59:33 +0200

    chore: Skip optimizing site

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ export FONTCONFIG_PATH=${PWD}/assets/fonts
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.30.0
 /opt/buildhome/.deno/bin/deno task build
 npm run generate:og
-npm run optimize:site
+# npm run optimize:site
