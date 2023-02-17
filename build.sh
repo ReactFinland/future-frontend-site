93681cb744dda5f749d8a35f0944ea3a2f16c226
Juho Vepsäläinen <bebraw@gmail.com>
2023-02-17 18:36:53 +0200

    refactor: Simplify deploy script

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+# Stop deploying on error
+# https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#The-Set-Builtin
+set -e
 
 export FONTCONFIG_PATH=${PWD}/assets/fonts
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.30.0
-/opt/buildhome/.deno/bin/deno task build || exit 1;
-npm run generate:og || exit 1;
-npm run optimize:site || exit 1;
+/opt/buildhome/.deno/bin/deno task build
+npm run generate:og
+npm run optimize:site
