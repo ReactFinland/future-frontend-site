3e8094beea5cf3cf9891b9edf43e98e710087923
Juho Vepsäläinen <bebraw@gmail.com>
2023-02-17 18:33:16 +0200

    chore: Cancel cloudflare build on failure

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 export FONTCONFIG_PATH=${PWD}/assets/fonts
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.30.0
-/opt/buildhome/.deno/bin/deno task build
-npm run generate:og
-npm run optimize:site
+/opt/buildhome/.deno/bin/deno task build || exit 1;
+npm run generate:og || exit 1;
+npm run optimize:site || exit 1;
