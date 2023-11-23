5da1ded0cf969a9a37896db4903fa8a338161a96
Juho Vepsäläinen <bebraw@gmail.com>
2023-11-23 11:55:56 +0200

    refactor: Push og generation to build

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@
 set -e
 
 export FONTCONFIG_PATH=${PWD}/assets/fonts
-curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.38.0
+curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.38.2
 /opt/buildhome/.deno/bin/deno task build
-npm run generate:og
 npm run optimize:site
