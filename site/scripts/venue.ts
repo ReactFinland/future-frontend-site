d0c5c75c1d5e6e65cf85c8d510a56337064bdf9c
Juho Vepsäläinen <bebraw@gmail.com>
2024-09-16 11:28:56 +0300

    chore: Improve map

--- site/scripts/venue.ts
+++ site/scripts/venue.ts
@@ -3,8 +3,18 @@ import "https://cdn.jsdelivr.net/npm/leaflet@1.9.3/dist/leaflet-src.min.js";
 function renderMap(id: string) {
   // @ts-expect-error How to type Leaflet?
   const L = window.L;
-  const venueLatLon = [60.1851476, 24.8297228];
+  const venueLatLon = [60.18500145319482, 24.83247578144074];
+  const hotelLatLon = [60.18371611765855, 24.83635213466187];
+  const hotel2LatLon = [60.17899837467085, 24.82998408489948];
   const map = L.map(id).setView(venueLatLon, 12);
+
+  // Debug
+  /*
+  map.on("click", function (e) {
+    console.log(e.latlng.lat);
+    console.log(e.latlng.lng);
+  });
+  */
 
   // Different options at https://leaflet-extras.github.io/leaflet-providers/preview/
   L.tileLayer(
@@ -19,8 +29,14 @@ function renderMap(id: string) {
 
   L.marker(venueLatLon).addTo(map)
     // .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-    .bindPopup("Dipoli")
+    .bindPopup("Dipoli - the venue")
     .openPopup();
+
+  L.marker(hotelLatLon).addTo(map)
+    .bindPopup("Radisson Blu Hotel (****)");
+
+  L.marker(hotel2LatLon).addTo(map)
+    .bindPopup("Heymo 1 Hotel (****)");
 }
 
 renderMap("venue-map");
