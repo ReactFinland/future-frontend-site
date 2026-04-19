7d8750d3f92d86167bb42664ec0c5db71e0821c2
Juho Vepsäläinen <bebraw@gmail.com>
2026-04-19 22:04:13 +0300

    Restore Twind and Markdown rendering

--- site/components/Markdown.server.ts
+++ site/components/Markdown.server.ts
@@ -15,6 +15,10 @@ function normalizeMarkdownInput(input: unknown) {
     return input;
   }
 
+  if (Array.isArray(input)) {
+    return input.map(normalizeMarkdownInput).join("");
+  }
+
   if (
     input &&
     typeof input === "object" &&
@@ -24,7 +28,34 @@ function normalizeMarkdownInput(input: unknown) {
     return input.content;
   }
 
+  if (isRawHtml(input)) {
+    return input.value;
+  }
+
+  if (
+    input &&
+    typeof input === "object" &&
+    "value" in input
+  ) {
+    return normalizeMarkdownInput(input.value);
+  }
+
+  if (
+    input &&
+    typeof input === "object" &&
+    "items" in input &&
+    Array.isArray(input.items)
+  ) {
+    return input.items.map(normalizeMarkdownInput).join("");
+  }
+
   return "";
+}
+
+function isRawHtml(input: unknown): input is { __htmlispRaw: true; value: string } {
+  return !!input && typeof input === "object" &&
+    "__htmlispRaw" in input && input.__htmlispRaw === true &&
+    "value" in input && typeof input.value === "string";
 }
 
 export { init };
